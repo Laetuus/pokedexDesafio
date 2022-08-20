930a7d44c3ba6fe2f2c1cc85c881181f3064ebea
Laetuus <dwalexdw@gmail.com>
2022-08-20 19:39:58 -0300

    Estrutura JS da pokedex, referente aos botões e ao campo de pesquisa de pokemon.

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,59 @@
 const pokemonName = document.querySelector('.nomepokemon');
 const pokemonNumber = document.querySelector('.numeropokemon');
+const pokemonImage = document.querySelector('.imagem_pokemon');
+
+const form = document.querySelector('.form');
+const input = document.querySelector('#nomepokemon')
+const botao1 = document.querySelector('#botao1');
+const botao2 = document.querySelector('#botao2');
+
+let searchPokemon = 1;
 
 const fetchPokemon = async (pokemon) => {
     const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
-    const data = await APIResponse.json();
+    
+
+    if (APIResponse.status == 200) {
+        const data = await APIResponse.json();
     return data;
+    }
 }
 
 const renderPokemon = async (pokemon) => {
+
+    pokemonName.innerHTML = 'Procurando...';
+    pokemonNumber.innerHTML = ''
+    
     const data = await fetchPokemon(pokemon);
 
-    pokemonName.innerHTML = data.name;
-    pokemonNumber.innerHTML = data.id;
+    if (data) {
+        pokemonName.innerHTML = data.name;
+        pokemonNumber.innerHTML = data.id;
+        pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+        input.value = '';
+        searchPokemon = data.id;
+    } else {
+        pokemonImage.style.display = 'none';
+        pokemonName.innerHTML = 'Not Found o(╥﹏╥)o';
+        pokemonNumber.innerHTML = '';
+    }
 }
 
-renderPokemon('charizard')
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    renderPokemon(input.value.toLowerCase());  
+});
+
+botao1.addEventListener('click', () => {
+    if(searchPokemon > 1){
+    searchPokemon -= 1;
+    renderPokemon(searchPokemon);
+    } 
+});
+
+botao2.addEventListener('click', () => {
+    searchPokemon += 1;
+    renderPokemon(searchPokemon);
+});
+
+renderPokemon(searchPokemon);
